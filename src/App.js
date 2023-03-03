705d6701853e28a097d80a988dae84fc51940b85
DESKTOP-BJPK43B\user <hootingowl09@naver.com>
2023-03-03 21:49:29 +0900

    Feat: 단어장 만들기 연습

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
