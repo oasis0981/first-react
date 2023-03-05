c33c3723f4396a41a62e35ebc532e20182e7dfa6
DESKTOP-BJPK43B\user <hootingowl09@naver.com>
2023-03-05 21:10:49 +0900

    Fix: tsx 인식 오류 해결

--- src/component/Day.jsx
+++ src/component/Day.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import Word from "./Word";
+import Word from "./Word.tsx";
 import useFetch from "../hooks/useFetch";
 
 export default function Day() {
